cf20fb9875c5bbbf9a9ca25076a60129ae3edb12
daejung2613-blip <daejung2613@dge.go.kr>
2026-09-03 10:26:37 +0900

    Add files via upload

---
+++ Program0903-2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores;
+            scores = new int[5];
+            scores[1] = 5;
+            Console.WriteLine(scores[0]);
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+            float[] data_float;
+            data_float = new float[3] { 1.1f, 2.2f, 3.3f };
+
+            string[] data_str = new string[3] { "aaa", "bbb", "ccc" };
+
+            // 3. .Length 추가
+            for (int i = 0; i < data_float.Length; i++)
+            {
+                Console.WriteLine(data_float[i] + data_str[i]);
+            }
+            foreach (string v in data_str)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
